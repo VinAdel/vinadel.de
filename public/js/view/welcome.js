509d5c16ab2d707b75477faf0a34da5169bd58fe
VinAdel <vinadel@outlook.de>
2021-02-11 00:18:36 +0100

    production version

--- public/js/view/welcome.js
+++ public/js/view/welcome.js
@@ -1,15 +1,1 @@
-/******/ (() => { // webpackBootstrap
-/*!**************************************!*\
-  !*** ./resources/js/view/welcome.js ***!
-  \**************************************/
-$(document).on("click", ".code > .menu > div:not(.copy)", function () {
-  $(".code > .menu > div").removeClass("active");
-  $(this).addClass("active");
-
-  if (!$(".code > div." + $(this).data("show")).is(":visible")) {
-    $(".code > div:not(.menu)").fadeOut(500);
-    $(".code > div." + $(this).data("show")).delay(500).fadeIn();
-  }
-});
-/******/ })()
-;
+$(document).on("click",".code > .menu > div:not(.copy)",(function(){$(".code > .menu > div").removeClass("active"),$(this).addClass("active"),$(".code > div."+$(this).data("show")).is(":visible")||($(".code > div:not(.menu)").fadeOut(500),$(".code > div."+$(this).data("show")).delay(500).fadeIn())}));
